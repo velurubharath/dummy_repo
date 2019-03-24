c5a0298070728966f511c7779bd882d614775b71
velurubharath <velurubharath@gmail.com>
2019-03-24 11:57:39 +0530

    formJquery

--- jquery/index.js
+++ jquery/index.js
@@ -1,6 +1,28 @@
 $(document).ready(() => {
 
-    let myText = $('body').html();
-    alert(myText)
+    $(username).trigger("focus")
+
+    $("#performAction").click(()=>{
+        alert("Button is clicked")
+        console.log("Button is clicked")
+    })
+
+    $("#newTag").dblclick(()=>{
+        alert("Blink is double clicked")
+        console.log("link is double clicked")
+    })
+
+    $('form').submit(()=>{
+        alert("Form is submitted")
+    })
+
+    $('input').focus(()=>{
+        $(this).addClass("border-input");
+    })
+
+    $('input').focusOut(()=>{
+        $(this).removeClass("border-input");
+    })
     
 })//end doc ready
+
